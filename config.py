cf795232d8638ce199c5c0f87755e4bf8e2e34dc
Timur <isopevevehy.ru@gmail.com>
2025-06-21 12:15:58 +0500

    Финальные изменения

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-SECRET_KEY = os.urandom(24)
+SECRET_KEY = 'your-secret-key'  # Например, сгенерируй на https://djecrety.ir/
 SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = 'static/portfolio'
-RECAPTCHA_SECRET_KEY = '6Lc7ZWgrAAAAALKvR4nIXoBKCdzYNjgKgEiH2feq'
+RECAPTCHA_SECRET_KEY = '6LfnbWgrAAAAAOZUjf91CPAjBoWoFVDQGxxR0QZ1'
